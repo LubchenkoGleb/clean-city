9e6ded36e8a7cf5c87344c9d53e8934f51187718
gleb.lubchenko <gleb.lubchenko@gmail.com>
2017-08-08 13:24:08 +0300

    test heroku behavior during failing test

--- src/test/java/com/kpi/diploma/smartroads/SmartRoadsApplicationTests.java
+++ src/test/java/com/kpi/diploma/smartroads/SmartRoadsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kpi.diploma.smartroads;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SmartRoadsApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testHerokuBehaviorDuringFailingTest() {
+		Assert.assertTrue(false);
 	}
 
 }
